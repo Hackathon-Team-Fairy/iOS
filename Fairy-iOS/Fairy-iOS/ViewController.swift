6a73703a670289415a4e9ece6fa53656089977f3
mandos1995 <mandos19950620@gmail.com>
2023-06-10 13:54:07 +0900

    🎉 프로젝트 생성

---
+++ Fairy-iOS/Fairy-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fairy-iOS
+//
+//  Created by 김민석 on 2023/06/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
