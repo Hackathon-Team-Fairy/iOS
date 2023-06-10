353610a6173942d2374c98b2b20d15d557691298
mandos1995 <mandos19950620@gmail.com>
2023-06-10 18:42:16 +0900

    chore: 에셋 추가 및 이미지 네임스페이스 작성

---
+++ Fairy-iOS/Fairy-iOS/Utils/Constant/Constant.swift
@@ -0,0 +1,14 @@
+//
+//  Constant.swift
+//  Fairy-iOS
+//
+//  Created by 김민석 on 2023/06/10.
+//
+
+import Foundation
+
+enum ImageNameSpace: String {
+    case emptyImage
+    case loadingImageView
+    case navigationBackButton
+}
