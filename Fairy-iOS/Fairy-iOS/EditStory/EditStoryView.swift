97e3bbac3531cbe8ba4628730dd096a549d9c993
mininim <minsoplee2015@gmail.com>
2023-06-10 21:09:38 +0900

    [FEAT] EditStoryViewController 구현
    navigation 연결

--- Fairy-iOS/Fairy-iOS/EditStory/EditStoryView.swift
+++ Fairy-iOS/Fairy-iOS/EditStory/EditStoryView.swift
@@ -6,24 +6,52 @@
 //
 
 import SwiftUI
+import SnapKit
 
 struct EditStoryView: View {
     @State var title: String = ""
     
-    @State var stroy1: String = "‘새로운 팀원, 새로운 도전' 옛날 어느 마을에 한 작가가 살고 있었다. 이 작가는 매일같이 새로운 도전을 받아들이며 그것을 극복하며 살아가고 있었다. 어느 날 해커톤 참가 신청을 하게 되어 새로운 팀원들을 만나게 되었다   도전을 받아들이며 그것을 극복하며 살아가고 있었다. 어느 날 해커톤 참가 신청을 하게 되어 새로운 팀원들을 만나게 되었다 도전을 받아들이며 그것을 극복하며 살아가고 있었다. 어느 날 해커톤 참가 신청을 하게 되어 새로운 팀원들을 만나게 되었다 "
+    @State var story1: String = "‘새로운 팀원, 새로운 도전' 옛날 어느 마을에 한 작가가 살고 있었다. 이 작가는 매일같이 새로운 도전을 받아들이며 그것을 극복하며 살아가고 있었다. 어느 날 해커톤 참가 신청을 하게 되어 새로운 팀원들을 만나게 되었다   도전을 받아들이며 그것을 극복하며 살아가고 있었다. 어느 날 해커톤 참가 신청을 하게 되어 새로운 팀원들을 만나게 되었다 도전을 받아들이며 그것을 극복하며 살아가고 있었다. 어느 날 해커톤 참가 신청을 하게 되어 새로운 팀원들을 만나게 되었다 "
+    @State var story2: String = "신데렐라는 어려서"
+    @State var story3: String = "백마탄 왕자님 두둥등장"
+    @State var story4: String = "가보작오"
     
     var body: some View {
-        VStack{
-            EditTipView()
-            
-            EditTitleView(name: $title)
-            
-            EditTextView(story: $stroy1)
+        NavigationView {
+            VStack{
+                EditTipView()
+                
+                EditTitleView(name: $title)
+                
+                EditTextTabView(story1: $story1, story2: $story2, story3: $story3, story4: $story4)
+                
+                
+//                Button {
+//                    <#code#>
+//                } label: {
+//                    <#code#>
+//                }
+
+                
+//                NavigationLink(destination: Text("오")) {
+//                    ZStack{
+//                        RoundedRectangle(cornerRadius: 15)
+//                            .foregroundColor(.gray)
+//                            .frame(width: 319, height: 50)
+//                        Text("표지 만들러가기")
+//                    }
+//                }
+
+                
+                Spacer()
+            }
+            .onTapGesture {
+                self.endTextEditing()
+            }
         }
-        .onTapGesture {
-            self.endTextEditing()
-        }
+        
     }
+    
     
     
 }
@@ -32,17 +60,21 @@ struct EditTipView: View {
     
     var body: some View {
         
-        ZStack{
-            RoundedRectangle(cornerRadius: 8)
-                .foregroundColor(.gray)
-                .frame(width: 355, height: 58)
-            
-            
+        VStack{
             HStack{
-                Image(systemName: "book.fill")
-                Text("이야기를 자유롭게 편집해주세요")
+                Text("이야기 내용을 편집해주세요")
+                    .foregroundColor(.black)
+                    .font(.system(size: 18))
+                Spacer()
+            }
+            HStack{
+                Text("자유롭게 편집하며 나만의 이야기를 만들어보세요")
+                    .foregroundColor(.gray)
+                    .font(.system(size: 14))
+                Spacer()
             }
         }
+        .padding(.horizontal)
     }
 }
 
@@ -75,6 +107,30 @@ struct EditTitleView: View {
     }
 }
 
+struct EditTextTabView: View{
+    @Binding var story1: String
+    @Binding var story2: String
+    @Binding var story3: String
+    @Binding var story4: String
+    
+    var body: some View {
+        
+        GeometryReader { geo in
+            TabView {
+                EditTextView(story: $story1)
+                EditTextView(story: $story2)
+                EditTextView(story: $story3)
+                EditTextView(story: $story4)
+            }
+            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .always))
+            .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
+            .indexViewStyle(PageIndexViewStyle())
+        }
+        .frame(height: 500)
+        
+        
+    }
+}
 
 struct EditTextView: View {
     @Binding var story: String
@@ -115,23 +171,3 @@ struct EditStoryView_Previews: PreviewProvider {
     }
 }
 
-
-class EditStoryViewController: UIViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        // SwiftUI 화면을 UIKit에 통합
-        let contentView = EditStoryView()
-        let hostingController = UIHostingController(rootView: contentView)
-        addChild(hostingController)
-        view.addSubview(hostingController.view)
-        hostingController.view.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            hostingController.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            hostingController.view.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            hostingController.view.topAnchor.constraint(equalTo: view.topAnchor),
-            hostingController.view.bottomAnchor.constraint(equalTo: view.bottomAnchor)
-        ])
-        hostingController.didMove(toParent: self)
-    }
-}
