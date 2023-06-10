a7e01160d0a05ea4649178595969c4596cf33509
mininim <minsoplee2015@gmail.com>
2023-06-10 22:00:58 +0900

    [FEAT] PhotoSelectViewController 하단 완성하기버튼 구현

--- Fairy-iOS/Fairy-iOS/PhotoSelect/PhotoSelectViewController.swift
+++ Fairy-iOS/Fairy-iOS/PhotoSelect/PhotoSelectViewController.swift
@@ -7,8 +7,18 @@
 
 import UIKit
 import SwiftUI
+import SnapKit
 
 class PhotoSelectViewController: UIViewController {
+    private let completeButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .gray
+        button.layer.cornerRadius = 15
+        button.setTitle("완성하기", for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 18)
+        
+        return button
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,7 +35,21 @@ class PhotoSelectViewController: UIViewController {
             hostingController.view.topAnchor.constraint(equalTo: view.topAnchor),
             hostingController.view.bottomAnchor.constraint(equalTo: view.bottomAnchor)
         ])
+        view.addSubview(completeButton)
+        completeButton.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview().inset(28)
+            $0.height.equalTo(50)
+            $0.bottom.equalToSuperview().offset(-49)
+        }
+        completeButton.addTarget(self, action: #selector(selectButtonTouchUpInside), for: .touchUpInside)
+        
         hostingController.didMove(toParent: self)
+    }
+    
+    @objc
+    private func selectButtonTouchUpInside() {
+        let creatingFairyTaleViewController = PhotoSelectViewController()
+        navigationController?.pushViewController(creatingFairyTaleViewController, animated: true)
     }
     
 }
